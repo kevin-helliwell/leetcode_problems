0bb4b994321db02339213038802f103c0d2a099f
Kevin Helliwell <kbh789@gmail.com>
2023-03-02 11:06:30 -0500

    add clarifying comments

--- partition_labels_js/main.js
+++ partition_labels_js/main.js
@@ -4,8 +4,13 @@
 
 // Return a list of integers representing the size of these parts.
 
+
 const partitionLabels = (string) => {
     const lastSeenIndices = {};
+
+    // Iterates through the string to find the last index of each character
+    // Starts at right and moves left because last index on left is equivalent to first index on right (optimization)
+    // Only maps char (key) to i (value) if char doesn't already have an associated value (optimization)
 
     for (let i = string.length - 1; i >= 0; i--) {
         const char = string[i];
@@ -16,15 +21,19 @@ const partitionLabels = (string) => {
     }
 
     const partitions = [];
-    let start = 0;
-    let end = 0;
+    let start = 0; // start index of a partition; starts at 0, only changes if new partition is created
+    let end = 0; // end index of a partition; starts at 0, and changes based on char being inspected
 
     for (let i = 0; i < string.length; i++) {
+        // if char at string[i] has a last index > current end, update end to lastSeenIndices[char]
         const char = string[i];
         const lastCharIndex = lastSeenIndices[char];
 
         if (lastCharIndex > end) end = lastCharIndex;
 
+        // if i === end, partition has been found
+        // partition gets added to partitions array
+        // start gets updated, and iteration continues
         if (i === end) {
             const partition = end - start + 1;
             partitions.push(partition);
